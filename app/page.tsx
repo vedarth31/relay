30dc070967cc5f29ab99771cf07afe5cc84c524c
Athul Suresh <athulrsuresh@gmail.com>
2024-10-06 21:42:55 -0500

    some file stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,32 @@
-import Table from "./components/table/table";
+import prisma from "./lib/prisma"
+import OrderTable from "./components/order-table/order-table";
 
-export default function Dashboard(){
-    return Table();
+export default async function Dashboard(){
+    const orders = await prisma.order.findMany({
+        orderBy: {
+            createdAt: 'desc',
+        },
+        include: {
+            user: true,
+            items: true,
+        },
+    });
+    
+    const serializedOrders = orders.map((order) => ({
+        ...order,
+        createdAt: order.createdAt.toISOString(),
+        updatedAt: order.updatedAt.toISOString(),
+        user: {
+            ...order.user,
+            createdAt: order.user.createdAt.toISOString(),
+            updatedAt: order.user.updatedAt.toISOString(),
+        },
+        items: order.items.map((item) => ({
+            ...item,
+            createdAt: item.createdAt.toISOString(),
+            updatedAt: item.updatedAt.toISOString(),
+        })),
+    }));
+    
+    return <OrderTable orders={serializedOrders} />;
 }
