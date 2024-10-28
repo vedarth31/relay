60c5a33ee9e58e492394f531159b15772d478f81
vedarth31 <vedarth31@gmail.com>
2024-10-27 20:47:38 -0500

    removed prisma functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,38 @@
-import prisma from "./lib/prisma";
-import OrderTable from "./components/order-table/order-table";
-import Layout from "./components/layout/layout";
+// import prisma from "./lib/prisma";
+// import OrderTable from "./components/order-table/order-table";
+// import Layout from "./components/layout/layout";
 
 export default async function Dashboard(){
-    const orders = await prisma.order.findMany({
-        orderBy: {
-            createdAt: 'desc',
-        },
-        include: {
-            user: true,
-            items: true,
-        },
-    });
+    // const orders = await prisma.order.findMany({
+    //     orderBy: {
+    //         createdAt: 'desc',
+    //     },
+    //     include: {
+    //         user: true,
+    //         items: true,
+    //     },
+    // });
     
-    const serializedOrders = orders.map((order) => ({
-        ...order,
-        createdAt: order.createdAt.toISOString(),
-        updatedAt: order.updatedAt.toISOString(),
-        user: {
-            ...order.user,
-            createdAt: order.user.createdAt.toISOString(),
-            updatedAt: order.user.updatedAt.toISOString(),
-        },
-        items: order.items.map((item) => ({
-            ...item,
-            createdAt: item.createdAt.toISOString(),
-            updatedAt: item.updatedAt.toISOString(),
-        })),
-    }));
+    // const serializedOrders = orders.map((order) => ({
+    //     ...order,
+    //     createdAt: order.createdAt.toISOString(),
+    //     updatedAt: order.updatedAt.toISOString(),
+    //     user: {
+    //         ...order.user,
+    //         createdAt: order.user.createdAt.toISOString(),
+    //         updatedAt: order.user.updatedAt.toISOString(),
+    //     },
+    //     items: order.items.map((item) => ({
+    //         ...item,
+    //         createdAt: item.createdAt.toISOString(),
+    //         updatedAt: item.updatedAt.toISOString(),
+    //     })),
+    // }));
     
     return (
-        <Layout>
-            <OrderTable orders={serializedOrders} />
-        </Layout>
+        // <Layout>
+        //     <OrderTable orders={serializedOrders} />
+        // </Layout>
+        <div></div>
     );
 }
